42959b7a65f31dfdc4bb6a7215ffad4126d77907
Matheus <matheus.sabes@auaha.com.br>
2019-10-30 23:04:11 -0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+
+		<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="/word_press">Menu</a></li>
+                    <li><a href="/word_press/sobre/">Sobre</a></li>
+                    <li><a href="/word_press/contato/">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+
+          <!--footer word press -->
+          <?php wp_footer(); ?>
+        <!-- fechar footer word press -->
+	</body>
+</html>
